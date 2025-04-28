cbb5cb42a8a91be21a6b4cbfcb72d1df2c479d74
shanmukhavemulapalli <shanmukhavemulapalli@gmail.com>
2025-04-27 19:53:12 -0500

    time corecition

--- functions.php
+++ functions.php
@@ -1,7 +1,10 @@
 <?php
 function addAuditLog($userId, $actionType, $description) {
     global $con;
+
+    date_default_timezone_set('America/Chicago');
     $actionDate = date('Y-m-d H:i:s');
+
     $stmt = $con->prepare("INSERT INTO audit_log (USER_ID, ACTION_TYPE, ACTION_DATE, DESCRIPTION) VALUES (?, ?, ?, ?)");
     $stmt->bind_param("isss", $userId, $actionType, $actionDate, $description);
     $stmt->execute();
